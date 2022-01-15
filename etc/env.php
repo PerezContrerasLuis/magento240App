3e47880732c654379184dcff34a09f1786b05652
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-01-15 14:29:55 -0600

    Oter files of APP/

---
+++ etc/env.php
@@ -0,0 +1,80 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_xjsloc'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '80b0512fc8e2fc426a7e828410247a59'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'magento240',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '666_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '666_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'magento240.local'
+    ],
+    'install' => [
+        'date' => 'Fri, 14 Jan 2022 15:04:12 +0000'
+    ]
+];
