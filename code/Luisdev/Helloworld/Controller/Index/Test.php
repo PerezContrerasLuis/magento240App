106a3619c2b301ed1528babfb099fd6aa9b59f34
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-01-15 14:22:17 -0600

    Creacion de Ruta frontend  1.- cramos archivo routes.ml que apunta al controlador Test  2.- creamos controlador Test  3.- limpiamos cache  php bin/magento cache:clean  4.- revisamos que el modulo este respondiendo con la siguiente URL      magento2.local/helloworld/index/test  5.- OJO : helloworld es el id que se registro en el archivo routes

---
+++ code/Luisdev/Helloworld/Controller/Index/Test.php
@@ -0,0 +1,18 @@
+<?php
+namespace Luisdev\Helloworld\Controller\Index;
+class Test extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+	public function execute()
+	{
+		echo "Hello world!";
+		exit;
+	}
+}
