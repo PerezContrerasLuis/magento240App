45789fd18703c5aa685e462b826d3bb23c510bb9
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-01-19 13:00:34 -0600

    Crear una vista
    Crearemos una vista para el Frontend haciendo sudo del modulo creado anteriormente en Pfay: https://github.com/PerezContrerasLuis/magento240App/commit/f693d0c838a6d86dfc06aa7f9f74e30c4a3525ad
    
    1.- Creamos nuestra carpeta vista que contendrá nuestros layous y templates.
    2.- traemos nuestro template:
    	app/code/Pfay/Contacts/view/frontend/templates/test_index.phtml
    3.- Creaos nuestro Layout
    	app/code/Pfay/Contacts/view/frontend/layout/contacts_test_index.xml
    
    	OJO EL NOMBRE DEL ARCHIVO ESTARÁ COMPUESTO DE LA SIGUIENTE MANERA :
    	'<id_of_the_route>_<name_of_the_controller>_<name_of_the_action>
    
    4.- Modificamos nuestro controlador index:
    	Agregamos la linea  para cargar el layout : ->_view->loadLayout();
    	Agregamos la linea  para renderizarla : ->_view->renderLayout();
    5.- Compilamos y re asignamos permisos
    	php bin/magento setup:di:compile
    	chmod -R 777 var/ pub/
    
    6.- Verificamos cambios con la siguiente url :
    	http://magento240.local/contacts/test/index

--- code/Pfay/Contacts/Controller/Test/Index.php
+++ code/Pfay/Contacts/Controller/Test/Index.php
@@ -4,6 +4,7 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        die('test index');
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
     }
 }
