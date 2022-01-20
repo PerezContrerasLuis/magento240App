b9456772264a442199410c44161efe1cd89c1eed
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-01-20 09:58:55 -0600

    Creación de Menú Panel admin
    Creación de menu en barra de menus ADMIN magento2:
    
    1.- Creamos nuestro archivo de configuración para nuestro modulo :
    	app/code/Pfay/Contacts/etc/adminhtml/menu.xml
    2.- Creámoos nuestro archivo ruta para el nuevo modulo (menu | enlace )
    	app/code/Pfay/Contacts/etc/adminhtml/routes.xml
    3.- Creamos nuestro controlador
    	app/code/Pfay/Contacts/Controller/Adminhtml/Test/Index.php
    
    4.-  Actualizamos, recompilamos y damos permisos
    	php bin/magento setup:upgrade
    	php bin/magento setup:di:compile
    
    	+------------------------------+
        	| chown www-data:www-data -R . | <------- OPCIONAL
    	+------------------------------+
        	chmod -R 777 var/ pub/

---
+++ code/Pfay/Contacts/Controller/Adminhtml/Test/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pfay\Contacts\Controller\Adminhtml\Test;
+use Magento\Backend\App\Action;
+
+class Index extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+        die('test admin view');
+    }
+}
