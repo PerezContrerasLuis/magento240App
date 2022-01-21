076501b017272fc7ddb83f825d73de63ad79a75b
PerezContrerasLuis <luisperezcontreras@gmail.com>
2022-01-21 10:27:38 -0600

    Creación de lista UIComponent en administrador de Magento P 2/3
    Agregamos accion de boton (Add a new contact), por ahora solo redirecciona a controlador e imprime  TEST ADD
    
    1.-  Nos aseguramos que se tenga agregado el item name=button en :
    	app/code/Pfay/Contacts/view/adminhtml/ui_component/contacts_test_listing.xml
    	la etiqueta url (*/*) indica que redirigiremos a la acción Agregar de la misma carpeta que nuestra Acción actual
    
    2.- Creamos controlador para esta acción :
    	app/code/Pfay/Contacts/Controller/Adminhtml/Test/Add.php
    
    3.- actualízamos , compilamos y asignamos permisos
    	php bin/magento setup:upgrade
    	php bin/magento setup:di:compile
    	chmod -R 777 var/ pub/

---
+++ code/Pfay/Contacts/Controller/Adminhtml/Test/Add.php
@@ -0,0 +1,11 @@
+<?php
+namespace Pfay\Contacts\Controller\Adminhtml\Test;
+use Magento\Backend\App\Action;
+
+class Add extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+       die('test add');
+    }
+}
