bd7582f02e1566b550459ddddbd6ce2ab7827691
PerezContrerasluis <luisperezcontreras@gmail.com>
2022-04-18 23:14:05 -0500

    Modulo para ejemplo Inyeccion de dependencias

---
+++ code/Luisdev/Inyeccion/Controller/Index/Index.php
@@ -0,0 +1,18 @@
+<?php
+namespace Luisdev\Inyeccion\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+	public function execute()
+	{
+		//return $this->_pageFactory->create();
+        echo "controlador Inyection";
+	}
+}
---
+++ code/Luisdev/Inyeccion/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Luisdev_Inyeccion',
+	__DIR__
+);
