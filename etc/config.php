36843423c82e42369226c2054a10e1b7808bd52f
PerezContrerasluis <luisperezcontreras@gmail.com>
2022-06-25 18:29:20 -0500

    Creacion de modulo para pruebas javascript

--- etc/config.php
+++ etc/config.php
@@ -290,6 +290,7 @@ return [
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'Foggyline_Plugged' => 1,
+        'JavaScriptDev_JsFun' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Onsitemessaging' => 1,
