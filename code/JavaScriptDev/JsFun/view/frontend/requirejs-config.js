7a8e84cf1bf4f39a5b2627f3665fbe5b58dee134
PerezContrerasluis <luisperezcontreras@gmail.com>
2022-06-26 18:40:04 -0500

    Agregando librerías de un CDN
    1.- incluimos contenido externo  con un CDN usando : paths
    2.- creamos nuestro archivo de piliticas de privacidad que permita hacer solicitudes	HTTP a CDNS externos  csp_whitelist.xml
    3.- creamos nuevo phtml que implementará VUE
    4.- creamos nuestro layout para el phtml  cms_index_index.xml
    
    Mas información de políticas de contenido seguro :  https://developer.adobe.com/commerce/php/development/security/content-security-policies/

--- code/JavaScriptDev/JsFun/view/frontend/requirejs-config.js
+++ code/JavaScriptDev/JsFun/view/frontend/requirejs-config.js
@@ -3,5 +3,8 @@ var config =  {
         "*":{
             "fadeInElement":"JavaScriptDev_JsFun/js/fade-in-element"
         }
+    },
+    "paths":{
+        "vue":"https://cdn.jsdelivr.net/npm/vue@2/dist/vue"
     }
 }
